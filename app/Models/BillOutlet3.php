5bf082b0b86405afda3b5ffb893d71026feefd3c
imdevedugame <119270897+imdevedugame@users.noreply.github.com>
2025-03-19 16:09:24 +0700

    update mysql_3

--- app/Models/BillOutlet3.php
+++ app/Models/BillOutlet3.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BillOutlet3 extends Model
 {
-    protected $connection = 'mysql_2';
+    protected $connection = 'mysql_3';
     protected $table = 'bills';
 
     protected $guarded = ['id'];
